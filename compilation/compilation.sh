0fcdb49fe608af7131007ed53af10f745c4c1cd6
jvignaud <jamyvignaud@gmail.com>
2018-07-27 10:28:11 +0200

    Compilation rendue plus rapide

--- compilation/compilation.sh
+++ compilation/compilation.sh
@@ -22,18 +22,19 @@ else
 				./waf --targets bin/arducopter-quad #compilation dans le repertoire build/navio2/bin/
 				echo ""
 				cd compilation
+				echo 'Kill du process arducopter qui tourne sur le RPi'
 				chmod +x .kill_process.sh #on rend le fichier executable
-				chmod +x .reboot_drone.sh #on rend le fichier executable
-				#on kill le processus arducopter qui tourne en arriere plan et on redemarre le drone apres les executions qui viennent juste apres
-				#on ouvre sur un autre terminal
-				xterm -e "echo 'Kill du process arducopter qui tourne sur le RPi';./.kill_process.sh;sleep 3;./.reboot_drone.sh " & 
-				sleep 2
+				./.kill_process.sh
+
+				#on kill le processus arducopter qui tourne en arriere plan, on compile et on redemarre le drone (en ouvrant sur un autre terminal)
 				echo "Envoi des fichiers vers la RPi"
 				chmod +x .envoi_drone.sh #on rend le fichier executable
 				./.envoi_drone.sh #on envoie le fichier au drone arducopter-quad pendant que le fichier executer en arriere plan ne tourne plus et avant que le drone redemarre
 				chmod +x .envoi_fichier.sh #on rend le fichier executable
 				./.envoi_fichier.sh #on envoie le fichier parametre au drone pendant que le fichier executer en arriere plan ne tourne plus et avant que le drone redemarre
-				sleep 4
+				chmod +x .reboot_drone.sh #on rend le fichier executable
+				xterm -e "./.reboot_drone.sh" &
+				sleep 2
 				pkill xterm #on ferme l autre terminal
 
 				echo "Compilation et envoi du fichier terminés"
@@ -61,14 +62,16 @@ else
 			cd compilation
 			chmod +x .kill_process.sh #on rend le fichier executable
 			chmod +x .reboot_drone.sh #on rend le fichier executable
-			#on kill le processus arducopter qui tourne en arriere plan et on redemarre le drone apres les executions qui viennent juste apres
-			#on ouvre sur un autre terminal
-			xterm -e "echo 'Kill du process arducopter qui tourne sur le RPi';./.kill_process.sh;sleep 7;./.reboot_drone.sh " & 
-			sleep 5
-			echo "Envoi du fichier vers la RPi"
+
+			#on kill le processus arducopter qui tourne en arriere plan, on compile et on redemarre le drone (en ouvrant sur un autre terminal)
+			echo "Envoi des fichiers vers la RPi"
 			chmod +x .envoi_drone.sh #on rend le fichier executable
-			./.envoi_drone.sh #on envoie le fichier au drone pendant que le fichier executer en arriere plan ne tourne plus et avant que le drone redemarre
-			sleep 3
+			./.envoi_drone.sh #on envoie le fichier au drone arducopter-quad pendant que le fichier executer en arriere plan ne tourne plus et avant que le drone redemarre
+			chmod +x .envoi_fichier.sh #on rend le fichier executable
+			./.envoi_fichier.sh #on envoie le fichier parametre au drone pendant que le fichier executer en arriere plan ne tourne plus et avant que le drone redemarre
+			chmod +x .reboot_drone.sh #on rend le fichier executable
+			xterm -e "./.reboot_drone.sh" &
+			sleep 2
 			pkill xterm #on ferme l autre terminal
 
 			echo "Compilation et envoi du fichier terminés"
@@ -84,7 +87,7 @@ else
 		waf="$PWD/modules/waf/waf-light" #on donne un nom plus court au compilateur
 		./waf configure --board=navio2	#on configure pour le flight controler Navio2
 		echo "Compilation du fichier"
-		./waf --targets bin/arducopter-quad #compilation dans le repertoire build/navio2/bin/	
+		./waf --targets bin/arducopter-quad #compilation dans le repertoire build/navio2/bin/
 	fi
 		
 fi
