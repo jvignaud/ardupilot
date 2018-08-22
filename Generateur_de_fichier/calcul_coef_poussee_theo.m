375d55f893fe1d4344e507865652b5d39e08c188
jvignaud <39913472+jvignaud@users.noreply.github.com>
2018-08-22 21:00:51 +0200

    Update calcul_coef_poussee_theo.m

--- Generateur_de_fichier/calcul_coef_poussee_theo.m
+++ Generateur_de_fichier/calcul_coef_poussee_theo.m
@@ -1,7 +1,7 @@
 function coefficient_poussee = calcul_coef_poussee_theo(pas_helice,diametre_helice)
 %CALCUL_COEF_POUSSEE
-%   Fonction qui calcule le coefficient de poussée à partir des moindres
-%   au carrée, de la rotation des moteurs au carrée et de la poussée généré
+%   Fonction qui calcule le coefficient de poussÃ©e Ã  partir des moindres
+%   au carrÃ©e, de la rotation des moteurs au carrÃ©e et de la poussÃ©e gÃ©nÃ©rÃ©
 %   par un moteur
-coefficient_poussee = 28.35 * pas_helice * diametre_helice^3 * 10^(-10) * (pi/60)^2;
+coefficient_poussee = 28.35 * pas_helice * diametre_helice^3 * 10^(-10) * (2*pi/60)^2;
 end
